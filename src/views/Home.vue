fab050115e6bca9a53bc43359db6d7f62971431b
pream912 <37475546+pream912@users.noreply.github.com>
2022-08-01 18:03:58 +0530

    cute kevin

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Kevin castro"/>
+    <HelloWorld msg="Kevin castro Cute"/>
   </div>
 </template>
 
