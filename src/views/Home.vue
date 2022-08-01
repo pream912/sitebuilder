63cd4306400bd174c0ba1e1435cee70d25d4e13b
pream912 <preamkumar912@gmail.com>
2022-08-01 18:05:33 +0530

    raskal

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Kevin castro Cute"/>
+    <HelloWorld msg="Kevin castro Cute Raskal"/>
   </div>
 </template>
 
