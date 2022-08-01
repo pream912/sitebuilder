0f47b004f4b22bc8ce8a499618604a4aab2fa788
pream912 <preamkumar912@gmail.com>
2022-08-01 18:08:48 +0530

    kevin commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Kevin castro Cute Raskal"/>
+    <HelloWorld msg="Kevin castro Cute Raskal kevin kevin kevin"/>
   </div>
 </template>
 
